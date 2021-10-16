8d59d3701f75d28638cd4623b22182beab5f998d
KauanRdzin <89669838+KauanRdzin@users.noreply.github.com>
2021-10-15 23:48:09 -0300

    Create Q5part2.java

---
+++ Q5part2.java
@@ -0,0 +1,22 @@
+public class Q5_part {
+
+    public static void main(String[] args){
+
+        Q5_contato Kauan = new Q5_contato("Kauan Rodrigo Souza Damasceno", 99977343);
+
+        Kauan.enviar_mensagem("Olá kauan, sim sou eu o maniaco do parque e estou prestes a invandir sua casa, algum problema?");
+
+        Kauan.getQuant_naolidas();
+
+        Kauan.receber_mensagem("Sim po");
+        Kauan.receber_mensagem("Isso não é permitido segundo o Art. 157");
+        Kauan.receber_mensagem("Mas...");
+        Kauan.receber_mensagem("Acho que as pessoas tem seu livre arbitrio seja para serem babacas ou até maldosas mas consequencias vem...");
+        Kauan.receber_mensagem("então se quiser entre mas estarei com uma arma te esperando");
+
+        Kauan.getQuant_naolidas();
+
+        Kauan.mensagensNaoLidas();
+
+        Kauan.getQuant_naolidas();
+    }
